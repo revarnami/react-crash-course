a0c20dd267e7680549d0295cd3e4e90a23c58e66
Fauzi Arnami <fauzi.arnami@gmail.com>
2024-10-19 12:50:35 +0700

    adding onchange props in new post

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,16 @@
 import classes from './NewPost.module.css'
+import { useState } from 'react'
 
-function NewPost() {
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Document No</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={props.onDocNoChange}/>
       </p>
       <p>
         <label htmlFor="name">File Name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onFileNameChange}/>
       </p>
     </form>
   )
